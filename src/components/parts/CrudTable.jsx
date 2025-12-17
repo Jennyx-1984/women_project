73946b505d4cf62c0c9394ff2f9af9ad7d0eb5c3
marie charlotte <charlottedoulcet90@gmail.com>
2025-12-17 13:45:58 +0100

    conflicts solved

--- src/components/parts/CrudTable.jsx
+++ src/components/parts/CrudTable.jsx
@@ -1,39 +1,21 @@
-import CardActionButton from "../atomic/CardActionButton";
+import { BackgroundColor } from "@cloudinary/url-gen/actions/background/actions/BackgroundColor";
 
 const CrudTable = ({ db, setDataToEdit, deleteData }) => {
-  if (!Array.isArray(db) || db.length === 0)
-    return <p>No hay datos disponibles</p>;
+  if (!Array.isArray(db) || db.length === 0) return <p>No hay datos disponibles</p>;
 
   return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(auto-fill, 220px)",
-        gap: 16,
-      }}
-    >
+    <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fill, 620px)", gap: 16 }}>
       {db.map((post) => (
-        <div
-          key={post.id}
-          style={{ border: "1px solid #ddd", padding: 12 }}
-        >
-          <img
-            src={post.photo}
-            alt={post.text}
-            width="200"
-            height="140"
-            style={{ objectFit: "cover" }}
-          />
-          <p>
-            <strong>Título:</strong> {post.text}
+        <div key={post.id} style={{ border: "9px solid #000000ff", padding: 12, backgroundColor: "#fbfbfbff", borderRadius: 35, overflow: "hidden" }}>
+          <img src={post.photo} alt={post.text} width="300" height="300" style={{ objectFit: "cover", height: 340, width: 440, borderRadius: 32 }} />
+          <p style={{ fontSize: 48, margin: "10px 0 6px" }}>
+            <strong>Frase:</strong> {post.text}
           </p>
-          <p>
+          <p style={{ fontSize: 26, margin: 0 }}>
             <strong>Autor:</strong> {post.Author}
           </p>
           <button onClick={() => setDataToEdit(post)}>Editar</button>
-          <button onClick={() => deleteData(post.id)}>
-            Eliminar
-          </button>
+          <button onClick={() => deleteData(post.id)}>Eliminar</button>
         </div>
       ))}
     </div>
